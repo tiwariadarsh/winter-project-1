2960847d3a3eea41af4ad792c73d514d8727bc46
patiljayant <72199264+patiljayant@users.noreply.github.com>
2020-12-18 21:22:06 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { React, Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import './App.css';
+import Navbar from './views/navbar.js';
+import Home from './views/home.js';
+import Product from './views/product.js';
+import Cart from './views/cart.js';
+
+
+
+class App extends Component{
+  render(){
+    return (<BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/productsInCart' component={Cart}/>
+        <Route path='/products/:id'  component={Product}/>
+
+        </Switch>
+      </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
